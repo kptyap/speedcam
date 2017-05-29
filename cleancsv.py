79a42f26cc831f0c87067d13092b64e85f9bf24a
Keith Yap <kptyap@gmail.com>
2017-05-29 01:59:37 +0000

    First working to database

--- cleancsv.py
+++ cleancsv.py
@@ -2,24 +2,62 @@
 # 4. Insert into SQL db to be called by the app [ ]
 
 import csv
+import psycopg2
 
-search_for = ['Street Name', 'Brookton Highway']
 
-with open('KY.csv', 'rb') as fileObj:
-    readerObj = csv.reader (fileObj, delimiter=',')
-    for row in readerObj:
-    #    if row[0] in search_for:
-    #        print('Found: {}'.format(row))
+try:
+    conn = psycopg2.connect("dbname=speedcams user=ubuntu host=localhost password=Kypw123!")
+except:
+    print('Cannot connect to db')
+
+cur = conn.cursor()
+
+# copy pdf csv output into a clean instance of speedcamraw table
+cur.execute("TRUNCATE TABLE speedcamraw;"
+            "ALTER TABLE speedcamraw DROP COLUMN id;"
+            "COPY speedcamraw (street_name, suburb, street_name2, suburb2)"
+            "FROM '/home/ubuntu/workspace/speedcam/speedcam.csv' DELIMITER ',' CSV;"
+# create an autoincrementing primary key (id)
+# http://stackoverflow.com/questions/2944499/how-to-add-an-auto-incrementing-primary-key-to-an-existing-table-in-postgresql
+            "ALTER TABLE speedcamraw ADD id SERIAL PRIMARY KEY;"
+            )
+
+# check if suburb and street_name2 are empty, if so pull the PDF text date
+# http://stackoverflow.com/questions/23766084/best-way-to-check-for-empty-or-null-value
+cur.execute('''SELECT street_name, id FROM speedcamraw 
+            WHERE (((suburb = '') IS NOT FALSE) 
+            AND ((street_name2 = '') IS NOT FALSE));''')
             
-    #check number of columns?   
-        for column in row[0]:
-            print column[0]
-            
-        
-        #skip rows starting with 'Street Name,'
-        
-        
-        #format should be Street Name, Suburb
-        
-        
+datesfrompdf = cur.fetchall()
+for rowdate, rowid in datesfrompdf:
+    # delete all 'street name' header rows (ie. date header rows + 1)
+    cur.execute("DELETE FROM speedcamraw WHERE id = %s;", (rowid + 1,))
+                
+# Fill down dates based on date headers
+for rowdate, rowid in datesfrompdf:
+    cur.execute('''UPDATE speedcamraw SET got_date = %s
+                WHERE ((suburb IS NOT NULL) AND (street_name IS NOT NULL)
+                AND (id > %s));''', (rowdate, rowid))
+
+# Delete date header rows
+cur.execute('''DELETE FROM speedcamraw WHERE
+            ((suburb IS NULL) AND (street_name2 IS NULL)
+            AND (suburb2 IS NULL) AND (got_date IS NULL));''')
+
+# Copy date, streetname2 and suburb2
+cur.execute('''INSERT INTO speedcamraw (got_date, suburb, street_name)
+             SELECT got_date, suburb2, street_name2 FROM speedcamraw;''')
+
+# Clean up any blank rows
+cur.execute('''DELETE FROM speedcamraw WHERE
+            ((suburb IS NULL) AND (street_name IS NULL));''')
+             
+# Copy date, street_name and suburb into speedcamclean
+cur.execute('''INSERT INTO speedcamclean (date, suburb, street_name)
+            SELECT got_date, street_name, suburb FROM speedcamraw;''')
+
     
+conn.commit()
+
+cur.close()
+conn.close()
--- dlspeedcam.py
+++ dlspeedcam.py
@@ -3,12 +3,10 @@
 # 1. Download latest PDF on the WA Police website [x]
 # 2. Convert PDF into CSV [x]
 
-
 import tabula
 import urllib
 import os
 import datetime
-
 
 # set start date as the closest Monday prior, enddate as friday
 d = datetime.datetime.today().weekday()
@@ -17,6 +15,7 @@ ed1 = sd1 + datetime.timedelta(days=6)
 startdate1 = sd1.strftime('%d%m%Y')
 enddate1 = ed1.strftime('%d%m%Y')
 
+# Manually set the date here:
 #startdate1 = '24042017'
 #enddate1 = '30042017'
 
@@ -35,27 +34,3 @@ if (filesize > 25000):
                             "speedcam.csv",
                             pages="all",
                             output_format="csv")
-
-
-# download the latest speedcam pdf from WA Police using URLLIB2
-# refer to http://www.pythonforbeginners.com/python-on-the-web/how-to-use-urllib2-in-python/
-
-# optional: specify user-agent
-#r = requests.get(url)
-    
-#if (r.status_code == 200):
-#    file = "speedcamDL.pdf"
-#    f = open(file, "w")
-#    f.write(r.content)
-#    f.close()
-
-    # extract speedcam locations from the pdf into a csv using tabula
-#    tabula.convert_into("speedcamDL.pdf",
-#                        "speedcam.csv",
-#                        pages="all",
-#                        output_format="csv")
-
-#else:
-#    r.raise_for_status()
-
-# download the latest speedcam pdf from WA Police using URLLIB
