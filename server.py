73e58d0b5f40f86ca58a009b18e1b26aab648842
Keith Yap <kptyap@gmail.com>
2017-06-02 07:39:17 +0000

    Updated server.py

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=8080)
-#app.run(environ.get('PORT'))
+#app.run(host='0.0.0.0', port=8080)
+app.run(environ.get('PORT'))
 
 @app.route("/")
 def helloworld():
