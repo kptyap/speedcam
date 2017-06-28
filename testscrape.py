67a27e0c8d455af4a029646f538943a127d68a5a
Keith Yap <kptyap@gmail.com>
2017-06-28 02:26:48 +0000

    periodic update

--- testscrape.py
+++ testscrape.py
@@ -1,13 +1,21 @@
 from bs4 import BeautifulSoup
 import urllib
 import tempfile
+import re
 
-url = 'https://www.police.wa.gov.au/~/media/Files/Police/Traffic/Cameras/Camera-locations'
-print url
+baseurl = "https://www.police.wa.gov.au"
+url = 'https://www.police.wa.gov.au/Traffic/Cameras/Camera-locations'
 
-with tempfile.NamedTemporaryFile() as temp:
-    urllib.urlretrieve(url, temp.name)
+# retrieve the html of the page
+urllib.urlretrieve(url,'pol.html')
 
-    soup = BeautifulSoup(temp.name, 'html.parser')
+# open the html document in BS4
+with open ('pol.html', 'r') as p:
+    soup = BeautifulSoup(p, 'html.parser')
     
-    print(soup.prettify())
+    # search using BS4 for the first PDF link
+    pdfurl = soup.find(href=re.compile("/~/media/Files"))['href']
+    pdfurl = baseurl + str(pdfurl)
+    print(pdfurl)
+
+urllib.urlretrieve(pdfurl, 'speedcamDL.pdf')
